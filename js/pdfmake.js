2adfafdd19db6aa290aaeceeb3d00241c950a079
Jery <jeremie.giudicelli@gmail.com>
2024-02-21 14:05:31 -0400

    modif pdf

--- js/pdfmake.js
+++ js/pdfmake.js
@@ -124,6 +124,148 @@ function generatePDF() {
     }
   }
 
+  // ARTICLE 1
+
+  let nameConvention = "....................";
+  let codeConvention = "....................";
+
+  document.getElementById("nameConvention").value
+    ? (nameConvention = document.getElementById("nameConvention").value)
+    : null;
+  document.getElementById("codeConvention").value
+    ? (codeConvention = document.getElementById("codeConvention").value)
+    : null;
+
+  //  ARTICLE 2
+  let nameSalary = "....................";
+  let fonctionSalary = "....................";
+  let statusSalary = "....................";
+  let positionSalary = "....................";
+  let echelonSalary = "....................";
+  let coefSalary = "....................";
+  let levelSalary = "....................";
+  let beginDate = "....................";
+  let cityUrssaf = "....................";
+  let taskClient = "....................";
+
+  document.getElementById("nameSalary").value
+    ? (nameSalary = document.getElementById("nameSalary").value)
+    : null;
+  document.getElementById("fonctionSalary").value
+    ? (fonctionSalary = document.getElementById("fonctionSalary").value)
+    : null;
+  document.getElementById("statusSalary").value
+    ? (statusSalary = document.getElementById("statusSalary").value)
+    : null;
+  document.getElementById("positionSalary").value
+    ? (positionSalary = document.getElementById("positionSalary").value)
+    : null;
+  document.getElementById("echelonSalary").value
+    ? (echelonSalary = document.getElementById("echelonSalary").value)
+    : null;
+  document.getElementById("coefSalary").value
+    ? (coefSalary = document.getElementById("coefSalary").value)
+    : null;
+  document.getElementById("levelSalary").value
+    ? (levelSalary = document.getElementById("levelSalary").value)
+    : null;
+  document.getElementById("beginDate").value
+    ? (beginDate = document.getElementById("beginDate").value)
+    : null;
+  document.getElementById("cityUrssaf").value
+    ? (cityUrssaf = document.getElementById("cityUrssaf").value)
+    : null;
+  document.getElementById("taskClient").value
+    ? (taskClient = document.getElementById("taskClient").value)
+    : null;
+
+  // ARTICLE 3
+  let essaiPeriode = "....................";
+  let maxPeriode = "....................";
+
+  document.getElementById("essaiPeriode").value
+    ? (essaiPeriode = document.getElementById("essaiPeriode").value)
+    : null;
+  document.getElementById("maxPeriode").value
+    ? (maxPeriode = document.getElementById("maxPeriode").value)
+    : null;
+
+  // ARTICLE 4
+  let WorkPlace1 = "..........";
+  let WorkPlace2 = "";
+  let WorkPlace3 = "";
+
+  document.getElementById("WorkPlace1").value
+    ? (WorkPlace1 = document.getElementById("WorkPlace1").value)
+    : null;
+  document.getElementById("WorkPlace2").value
+    ? (WorkPlace2 = document.getElementById("WorkPlace2").value)
+    : null;
+  document.getElementById("WorkPlace3").value
+    ? (WorkPlace3 = document.getElementById("WorkPlace3").value)
+    : null;
+
+  // ARTICLE 5
+  let hourWeek = "..........";
+  let hourMonth = "..........";
+
+  document.getElementById("hourWeek").value
+    ? (hourWeek = document.getElementById("hourWeek").value)
+    : null;
+  document.getElementById("hourMonth").value
+    ? (hourMonth = document.getElementById("hourMonth").value)
+    : null;
+
+  // ARTICLE 6
+  let moneyMonth = "....................";
+
+  document.getElementById("moneyMonth").value
+    ? (moneyMonth = document.getElementById("moneyMonth").value)
+    : null;
+
+  // ARTICLE 9
+  let orgaOthers = null;
+  let orgaOthers2 = null;
+
+  document.getElementById("orgaOthers").value
+    ? (orgaOthers = document.getElementById("orgaOthers").value)
+    : null;
+  document.getElementById("orgaOthers2").value
+    ? (orgaOthers2 = document.getElementById("orgaOthers2").value)
+    : null;
+
+  // ARTICLE 15
+
+  function demarcheSejour() {
+    if (titleSejour != "..........") {
+      let line1 =
+        isGenre() +
+        nameSalary +
+        " s’engage à effectuer toutes les démarches administratives nécessaires afin de toujours posséder un titre de séjour valable. De plus, ";
+      let line2 =
+        isGenre() +
+        nameSalary +
+        " s’engage à fournir une copie de ce document à l’Employeur.\n";
+      let line3 =
+        "Le non-respect de cette disposition expose " +
+        isGenre() +
+        nameSalary +
+        " à un risque de rupture du contrat de travail dans le respect de la législation en vigueur. \n\n";
+      return line1 + line2 + line3;
+    }
+  }
+
+  // FIN CONTRAT
+  let lieuContrat = "....................";
+  let dateContrat = "....................";
+
+  document.getElementById("lieuContrat").value
+    ? (lieuContrat = document.getElementById("lieuContrat").value)
+    : null;
+  document.getElementById("dateContrat").value
+    ? (dateContrat = document.getElementById("dateContrat").value)
+    : null;
+
   var dd = {
     styles: {
       h1: {
@@ -237,7 +379,7 @@ function generatePDF() {
         style: "p",
       },
       {
-        text: "IL EST CONVENU ET ARRETE CE QUI SUIT :\n\n\n\n",
+        text: "IL EST CONVENU ET ARRETE CE QUI SUIT :",
         pageBreak: "after",
         style: "h2",
       },
@@ -247,7 +389,12 @@ function generatePDF() {
         style: "h3",
       },
       {
-        text: "Le présent contrat est régi par les dispositions légales, réglementaires et conventionnelles applicables à la Société. En l'espèce, le présent contrat est régi par les dispositions conventionnelles de la Convention Collective .................... , IDCC ....................  (indiquer le code). La Convention Collective s'applique sous réserve d'un changement d'activité, ou tout autre situation entraînant sa mise en cause et les conditions particulières du présent contrat.\n\n",
+        text:
+          "Le présent contrat est régi par les dispositions légales, réglementaires et conventionnelles applicables à la Société. En l'espèce, le présent contrat est régi par les dispositions conventionnelles de la Convention Collective " +
+          nameConvention +
+          ", IDCC " +
+          codeConvention +
+          ". La Convention Collective s'applique sous réserve d'un changement d'activité, ou tout autre situation entraînant sa mise en cause et les conditions particulières du présent contrat.\n\n",
         style: "p2",
       },
       {
@@ -257,6 +404,341 @@ function generatePDF() {
       {
         text: "ARTICLE 2. DÉFINITION DE L'ENGAGEMENT",
         style: "h3",
+      },
+      {
+        text: [
+          isGenre() + nameSalary + " est ",
+          civilite == "1" ? "engagée" : "engagé",
+          " par la Société en qualité de " +
+            fonctionSalary +
+            ", Statut " +
+            statusSalary +
+            ", Position " +
+            positionSalary +
+            ", Échelon " +
+            echelonSalary +
+            ", Coefficient " +
+            coefSalary +
+            ", Niveau " +
+            levelSalary,
+          "\nLe présent contrat est conclu pour une durée indéterminée et prend effet à compter du " +
+            beginDate +
+            "\n\n",
+          "La déclaration préalable à l'embauche de " +
+            isGenre() +
+            nameSalary +
+            "  a été remise à l'URSSAF de " +
+            cityUrssaf +
+            ".\n",
+          isGenre() +
+            nameSalary +
+            " est informé de son droit d'accès et de rectification des données collectées conformément à la loi n°78-17 du 6 janvier 1978.\n\n",
+          isGenre() +
+            nameSalary +
+            " s'engage à effectuer les missions qui lui sont confiées par l'Employeur conformément à son profil professionnel et aux besoins de l'entreprise.\n\n",
+          taskClient,
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 3. PÉRIODE D'ESSAI",
+        style: "h3",
+      },
+      {
+        text: [
+          "Conformément aux dispositions légales et conventionnelles, la période d'essai de " +
+            isGenre() +
+            nameSalary +
+            " est de " +
+            essaiPeriode +
+            ".\n\n",
+          "Conformément aux dispositions conventionnelles, la période d'essai pourra être prolongée pour une durée maximale de " +
+            maxPeriode +
+            ".\n\n",
+          "Au cours de la période d'essai, l'Employeur et " +
+            isGenre() +
+            nameSalary +
+            " peuvent prendre l'initiative de rompre le contrat, sans indemnité, selon les délais de prévenance légaux et conventionnels en vigueur :\n\n",
+        ],
+        style: "p2",
+      },
+      {
+        text: "- En cas de rupture du présent contrat à l’initiative de l’employeur :",
+        style: "p2",
+      },
+      {
+        ul: [
+          {
+            ul: [
+              "Salarié présent depuis moins de 8 jours : 24h",
+              "Salarié présent depuis 8 jours ou plus : 48h",
+              "Salarié présent depuis moins d’un mois : 2 semaines",
+              "Salarié présent depuis plus d’un mois : 1 mois",
+            ],
+          },
+        ],
+      },
+      {
+        text: "\n- En cas de rupture du présent contrat à l’initiative du salarié :",
+        style: "p2",
+      },
+      {
+        ul: [
+          {
+            ul: [
+              "Salarié présent depuis moins de 8 jours : 24h",
+              "Salarié présent depuis plus de 8 jours : 48h",
+            ],
+          },
+        ],
+      },
+      {
+        text: "ARTICLE 4. LIEU DE TRAVAIL",
+        style: "h3",
+        pageBreak: "before",
+      },
+      {
+        text: [
+          "Le lieu de travail de " +
+            isGenre() +
+            nameSalary +
+            " est fixé au :\n",
+          WorkPlace1 != ".........."
+            ? WorkPlace1 + "\n" + WorkPlace2 + "\n" + WorkPlace3 + "\n\n"
+            : "..........\n ..........\n .......... \n\n",
+          "Toutefois et selon les nécessités du service, l’employeur pourra demander à " +
+            isGenre() +
+            nameSalary +
+            " de réaliser des déplacements de manière temporaire dans les lieux nécessaires à l’accomplissement de sa profession. Ces déplacements ne pourront avoir pour conséquence le changement de résidence.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 5. DURÉE DU TRAVAIL",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " réalisera " +
+            hourWeek +
+            " hebdomadaires effectives, correspondant à ",
+          hourMonth + "h mensualisées.\n\n",
+          isGenre() + nameSalary + " pourra être " + civilite == "1"
+            ? "amenée"
+            : "amené",
+          +" à effectuer des heures supplémentaires à la demande de la Direction qui seront rémunérées conformément aux dispositions légales et conventionnelles en vigueur.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 6. RÉMUNÉRATION",
+        style: "h3",
+      },
+      {
+        text: [
+          "En contrepartie de l'accomplissement de ses missions, ",
+          isGenre() +
+            nameSalary +
+            " percevra une rémunération mensuelle de " +
+            moneyMonth +
+            "€ pour " +
+            hourMonth +
+            "h.\n",
+          hourMonth == "169"
+            ? "La rémunération indiquée prend en compte les heures supplémentaires mensualisées.\n\n"
+            : null,
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 7. MÉDECINE DU TRAVAIL",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " s'engage à se soumettre aux examens médicaux obligatoires réalisés par le service de médecine du travail, conformément à la législation en vigueur.\n" +
+            "De plus, " +
+            isGenre() +
+            nameSalary +
+            " bénéficie d'un suivi médical régulier assuré par le service de médecine du travail. Ce suivi médical vise à évaluer l'aptitude de ",
+          civilite == "1" ? "cette dernière " : "ce dernier ",
+          "à son poste de travail, à prévenir les risques professionnels, et à détecter toute altération de la santé en lien avec son activité professionnelle.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 8. CONGÉS PAYÉS",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " aura droit à des congés payés conformément aux dispositions légales et conventionnelles en vigueur.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 9. PROTECTION SOCIALE",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " bénéficie des régimes de retraite et de protection sociale complémentaires auxquels l’entreprise a adhéré, conformément aux dispositions légales et conventionnelles en vigueur.",
+          isGenre() +
+            nameSalary +
+            " accepte de manière expresse le prélèvement de sa quote-part de cotisations prévus par les différents régimes ainsi que leurs modifications éventuelles.\n\n",
+          "Les organismes auxquels l’entreprise adhère à ce jour sont :\n",
+        ],
+
+        style: "p2",
+      },
+      {
+        ul: [
+          "Retraite complémentaire : IRCOM - 20 avenue des Arawaks - 97205 FORT-DE-FRANCE CEDEX",
+          orgaOthers ? orgaOthers : null,
+          orgaOthers2 ? orgaOthers2 : null,
+        ],
+      },
+      {
+        text: "ARTICLE 10. ABSENCES",
+        style: "h3",
+        pageBreak: "before",
+      },
+      {
+        text: [
+          "Sauf cas de force majeure, ",
+          isGenre() +
+            nameSalary +
+            " s’engage à informer l’Employeur de toute absence à son poste de travail dans les 24h suivant l’absence et à fournir un justificatif adéquat dans les 48h. \n\n",
+          "En cas de prolongation d’un arrêt de travail initial, ",
+          isGenre() +
+            nameSalary +
+            " s’engage à prévenir l’Employeur et lui transmettre dans les 48h le certificat médical.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 11. RUPTURE DU CONTRAT DE TRAVAIL",
+        style: "h3",
+      },
+      {
+        text: [
+          "A l'issue de la période d’essai, le présent contrat se poursuivra pour une durée indéterminée. Toutefois l’Employeur ainsi que ",
+          isGenre() +
+            nameSalary +
+            ", séparément ou d’un commun accord, peuvent mettre fin au présent contrat, conformément aux dispositions légales et conventionnelles applicables à l'entreprise.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 12. DISCRÉTION ET LOYAUTÉ",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " s’engage à observer toutes les instructions et consignes particulières de travail qui lui seront données et à respecter une stricte obligation de discrétion sur tout ce qui concerne l’activité de l’entreprise.\n\n",
+          isGenre() +
+            nameSalary +
+            " s'engage à faire preuve de la plus grande loyauté envers l'Employeur pendant toute la durée du présent contrat et s'abstient de tout comportement, acte ou omission préjudiciable aux intérêts de l'Employeur durant toute la durée de la relation de travail et à l’issue de celle-ci.\n\n",
+          isGenre() +
+            nameSalary +
+            "s'engage à ne pas exercer d'activité concurrente, directe ou indirecte pendant la durée du contrat.",
+          civilite == "1" ? " Elle" : " Il",
+          " s'interdit également de détourner ou de tenter de détourner les clients, partenaires commerciaux ou employés de l'Employeur.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 13. DONNÉES PERSONNELLES",
+        style: "h3",
+      },
+      {
+        text: [
+          "Dans le cadre de l'exécution du présent contrat, l'Employeur est amené à collecter, traiter et conserver des données personnelles concernant " +
+            isGenre() +
+            nameSalary +
+            " \n\n",
+          "L'Employeur s'engage à collecter et à traiter les données personnelles de " +
+            isGenre() +
+            nameSalary,
+          " manière licite, loyale et transparente, conformément aux lois et réglementations en vigueur en matière de protection des données.\n\n",
+          "Les données personnelles de " +
+            isGenre() +
+            nameSalary +
+            " seront traitées dans le seul but d'administrer le contrat, de respecter les obligations légales et réglementaires, et de gérer les relations professionnelles entre l'Employeur et ",
+          isGenre() + nameSalary + "\n\n",
+          isGenre() +
+            nameSalary +
+            " bénéficie du droit d'accéder à ses données personnelles détenues par l'Employeur et de demander leur rectification en cas d'inexactitude.\n\n",
+          "Les données personnelles de " +
+            isGenre() +
+            nameSalary +
+            " seront conservées pendant la durée nécessaire à la réalisation des finalités pour lesquelles elles ont été collectées, et conformément aux obligations légales en matière de conservation.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 14. FRAIS PROFESSIONNELS",
+        style: "h3",
+        pageBreak: "before",
+      },
+      {
+        text: [
+          "L’employeur remboursera à " +
+            isGenre() +
+            nameSalary +
+            " les frais professionnels ",
+          civilite == "1" ? "qu'elle sera amenée " : "qu'il sera amené ",
+          "à engager dans l’exercice de ses fonctions et sur demande de son supérieur hiérarchique. Le remboursement se fera sur présentation des justificatifs correspondants et en fonction des barèmes applicables dans la Société.",
+        ],
+        style: "p2",
+      },
+      {
+        text: "ARTICLE 15. AUTRES DISPOSITIONS",
+        style: "h3",
+      },
+      {
+        text: [
+          isGenre() +
+            nameSalary +
+            " bénéficiera tous les deux (2) ans d’un entretien professionnel avec l’Employeur conformément à l’article L. 6315-1 du Code du travail.\n\n",
+          demarcheSejour(),
+          "Enfin, le salarié déclare être libre de tout engagement et n’être lié(e) par aucune clause de non-concurrence à un autre employeur.\n\n\n\n\n\n",
+          "Fait en double exemplaire,\n\n",
+          "À " + lieuContrat + ", le " + dateContrat + "\n\n\n",
+        ],
+        style: "p2",
+      },
+      {
+        columns: [
+          {
+            text: civilite == "1" ? "La Salariée," : "Le Salarié,",
+            bold: true,
+          },
+          {
+            text: "L’employeur,",
+            bold: true,
+          },
+        ],
+      },
+      {
+        columns: [
+          {
+            text: isGenre() + nameSalary,
+          },
+          {
+            text: nameEmployeur + ", " + qualityEmployeur,
+          },
+        ],
       },
     ],
   };
